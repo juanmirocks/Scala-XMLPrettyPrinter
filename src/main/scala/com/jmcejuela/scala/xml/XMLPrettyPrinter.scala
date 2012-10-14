379acffb09ca27fa9d503431a4d5e8eca7c50297
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-10-14 13:05:30 +0200

    Consistent doc

--- src/main/scala/com/jmcejuela/scala/xml/XMLPrettyPrinter.scala
+++ src/main/scala/com/jmcejuela/scala/xml/XMLPrettyPrinter.scala
@@ -23,9 +23,9 @@ import java.io.Writer
  *    These are written exactly as they are read, with all the white spaces and new lines.
  *    All the other elements are completely stripped of leading & trailing whitespaces.
  *
- *    Consider for instance HTML-like documents (@see http://dev.w3.org/html5/html-xhtml-author-guide/)
- *    You can have the same behaviour as with the HTML <pre> tag.
- *    You could also have inlined <span>'s within a <p>
+ *    Consider for instance [[http://dev.w3.org/html5/html-xhtml-author-guide/ HTML-compatible XHTML documents]].
+ *    You can have the same behavior as with the HTML <pre> tag.
+ *    You could also have inlined <span>'s within a <p> without creating spurious break lines.
  *
  *  3. Thread safe: you can have the same (global) object used by different clients in parallel.
  *  4. Not tested, but presumably more efficient in both speed and space
