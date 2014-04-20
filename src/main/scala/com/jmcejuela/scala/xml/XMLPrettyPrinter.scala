9e5a935b202b8b1f3df5f2e138145f6c03f08d4d
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2014-04-20 12:17:35 +0200

    consistent documentation

--- src/main/scala/com/jmcejuela/scala/xml/XMLPrettyPrinter.scala
+++ src/main/scala/com/jmcejuela/scala/xml/XMLPrettyPrinter.scala
@@ -18,8 +18,8 @@ import java.io.Writer
  *
  * Advantages over scala.xml.PrettyPrinter:
  *
- *  1. You can both pretty-format a String or pretty-write directly to a file.
- *  2. pre-formatted elements: specify which nodes you want to be considered as pre-formatted.
+ * 1. You can both pretty-format a String or pretty-write directly to a file.
+ * 2. pre-formatted elements: specify which nodes you want to be considered as pre-formatted.
  *    These are written exactly as they are read, with all the white spaces and new lines.
  *    All the other elements are completely stripped of leading & trailing whitespaces.
  *
@@ -27,8 +27,9 @@ import java.io.Writer
  *    You write in XML with the same behavior as with the HTML <pre> tag.
  *    You could also have inlined <span>'s within a <p> without creating spurious break lines.
  *
- *  3. Thread safe: you can have the same (global) object used by different clients in parallel.
- *  4. Not tested, but presumably more efficient in both speed and space
+ * 3. Thread safe: you can have the same (global) object used by different clients in parallel.
+ * 4. Not tested, but presumably more efficient in both speed and space
+ *
  *
  * ## How to use
  * 
