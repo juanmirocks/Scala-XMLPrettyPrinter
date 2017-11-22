1ceae5aea791c1db647b5ca3d9925097b2e01791
Juan Miguel Cejuela <i@juanmi.rocks>
2017-11-22 11:04:56 +0100

    clean header doc

--- src/main/scala/rocks/juanmi/scala/xml/XMLPrettyPrinter.scala
+++ src/main/scala/rocks/juanmi/scala/xml/XMLPrettyPrinter.scala
@@ -22,28 +22,13 @@ import scala.xml.{Atom, Group, NamespaceBinding, Node, SpecialNode, Text}
 import scala.xml.dtd.DocType
 
 /**
- * # XMLPrettyPrinter
- *
- * Advantages over scala.xml.PrettyPrinter:
- *
- * 1. You can both pretty-format a String or pretty-write directly to a file.
- * 2. pre-formatted elements: specify which nodes you want to be considered as pre-formatted.
- *    These are written exactly as they are read, with all the white spaces and new lines.
- *    All the other elements are completely stripped of leading & trailing whitespaces.
- *
- * Consider for instance [Polyglot Markup documents](http://dev.w3.org/html5/html-polyglot/html-polyglot.html).
- * You can write in XML with the same behavior as with the HTML <pre> tag.
- * You could also have inlined <span>'s within a <p> without creating spurious break lines.
- *
- * 3. Thread safe: you can have the same (global) object used by different clients in parallel.
- * 4. Not tested, but presumably more efficient in both speed and space
+ * Utility to pretty-print Standard Scala XML.
+*
+ * Specially useful to have beautifully-formatted HTMLs or XHTMLs. You can pretty-print a scala (XML) Node to String or
+ * directly write it to a File for better efficiency.
  *
  *
- * ## How to use
- *
- * XMLPrettyPrinter is self-contained and has no dependencies.
- * Copy the [code](https://github.com/jmcejuela/Scala-XML-Pretty-Printer/blob/master/src/main/scala/com/jmcejuela/scala/xml/XMLPrettyPrinter.scala) and drop in to your project.
- *
+ * More info: https://github.com/juanmirocks/Scala-XML-Pretty-Printer
  *
  * @author Juan Miguel Cejuela (@juanmirocks)
  * @version 2.0.0
